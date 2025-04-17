87109735f2b7f02eda67b1fe1cf6a2f697c72e5b
Lucas Meier <lucas@cronokirby.com>
2025-04-16 20:21:57 -0700

    Add parsing for Anthropic API Responses
    This is a good checkpoint for now.

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,40 @@ module Main where
 
 import Ourlude
 
+import Data.Aeson (FromJSON (..), ToJSON (..))
+import qualified Data.Aeson as Aeson
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import GHC.Generics (Generic)
+
+-- | The possible responses returned by the Anthropic API.
+data ResponseContent
+  = TextResponse {text :: T.Text}
+  deriving (Show, Generic)
+
+responseContentJSONOptions :: Aeson.Options
+responseContentJSONOptions =
+  Aeson.defaultOptions
+    { Aeson.tagSingleConstructors = True
+    , Aeson.constructorTagModifier = \case
+        "TextResponse" -> "text"
+        x -> x
+    , Aeson.sumEncoding =
+        Aeson.TaggedObject
+          { Aeson.tagFieldName = "type"
+          , Aeson.contentsFieldName = "rest"
+          }
+    }
+
+instance ToJSON ResponseContent where
+  toJSON = Aeson.genericToJSON responseContentJSONOptions
+
+instance FromJSON ResponseContent where
+  parseJSON = Aeson.genericParseJSON responseContentJSONOptions
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  x <- T.getLine
+  let y = Aeson.decodeStrictText @ResponseContent x
+  print y
+  main
